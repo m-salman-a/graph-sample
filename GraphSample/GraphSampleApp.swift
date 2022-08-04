cbad035f5abab09407fdc7a96b8f7c94c358bde3
m salman a <mohammadalfarisi6@gmail.com>
2022-08-04 23:18:12 +0800

    Initial Commit

---
+++ GraphSample/GraphSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphSampleApp.swift
+//  GraphSample
+//
+//  Created by Mohammad Alfarisi on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
